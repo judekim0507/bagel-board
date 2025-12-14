d51ea320ac19289d4dbe184ae6bd4824ded1a793
Jude Kim <75868541+judekim0507@users.noreply.github.com>
2025-12-14 03:40:37 -0800

    fix: API keys in env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-const supabaseUrl = 'https://ajpzroepfitjngsfjkmu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqcHpyb2VwZml0am5nc2Zqa211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1OTY3NDQsImV4cCI6MjA4MTE3Mjc0NH0.-19Qiy_lNwyJdudsab2N36U3yryXJ7exAXAt9wsJ7L4';
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
